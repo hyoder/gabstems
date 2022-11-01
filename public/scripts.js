2a97c02fe1e2b255c19dd74cb45df1c9c0a780e2
hyoder <hryoder@wpi.edu>
2022-11-01 15:26:50 -0400

    fix readvals

--- public/scripts.js
+++ public/scripts.js
@@ -10,7 +10,7 @@ let    track1 = new Audio(),
         stems = false,
         speed = 1,
        colors = [ "#080806", "#977A74", "#EBE84D", "#EA3522", "#397326" ];
-var grd, t1src, t1anal, t1buff, t1data, t1width, t2src, t2anal, t2buff, t2data, t2width;
+var t1src, t1anal, t1buff, t1data, t1width, t2src, t2anal, t2buff, t2data, t2width;
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
 inputs.forEach( element => { 
@@ -31,7 +31,7 @@ track1.addEventListener('ended', () =>
     document.body.style.animationPlayState = 'paused';
     control.dataset.state = 'off';
     control.innerHTML = 'play again?';
-    grd = readvals();
+    readvals();
     console.log('ended');
 }, false );
 control.addEventListener('click', () => 
@@ -46,7 +46,7 @@ control.addEventListener('click', () =>
         document.body.style.animationPlayState = 'running';
         control.dataset.state = 'on';
         control.innerHTML = 'pause!';
-        grd = readvals();
+        readvals();
         track1.play();
         if( stems ) { track2.play(); }
         console.log('play');
@@ -56,7 +56,7 @@ control.addEventListener('click', () =>
         document.body.style.animationPlayState = 'paused';
         control.dataset.state = 'off';
         control.innerHTML = 'play!'
-        grd = readvals();
+        readvals();
         track1.pause();
         if( stems ) { track2.pause(); }
         console.log('pause');
@@ -69,7 +69,6 @@ function setsong(title)
     control.innerHTML = 'play!';
     track1.pause();
     if( stems ) { track2.pause(); }
-    grd = readvals();
     track1 = new Audio(),
     track2 = new Audio(),
     t1ctx = new window.AudioContext(),
@@ -133,7 +132,7 @@ function setsong(title)
         t2data  = new Uint8Array( t2buff ),
         t2width = canvas.width / t2buff;
     }
-    grd = readvals();
+    readvals();
     animate(); 
 }
 function animate()
@@ -178,30 +177,13 @@ function readvals()
     {
         tbl.innerHTML += '<td><label for="sp" style="font-family:\'Source Code Pro\',monospace;background-color:rgba(255,255,255,.6);text-align:center;">playback speed: </label>'
         tbl.innerHTML +=     '<input type="range" id="sp" value="' +  speed*4  + '" min="2" max="12" step="1" style="width:210px; height:25px;"></td>'
-        tbl.innerHTML += '<td><input type="color" id="c0" value="' + colors[0] + '" style="width:210px; height:25px; text-align:center;"></td>';
-        tbl.innerHTML += '<td><input type="color" id="c2" value="' + colors[2] + '" style="width:210px; height:25px; text-align:center;"></td>';
-        tbl.innerHTML += '<td><input type="color" id="c1" value="' + colors[1] + '" style="width:210px; height:25px; text-align:center;"></td>';
-        tbl.innerHTML += '<td><input type="color" id="c3" value="' + colors[3] + '" style="width:210px; height:25px; text-align:center;"></td>';
-        tbl.innerHTML += '<td><input type="color" id="c4" value="' + colors[4] + '" style="width:210px; height:25px; text-align:center;"></td>';
     }
     if( control.dataset.state === 'on' )
     {
             speed = document.getElementById( "sp" ).value /  4;
-        colors[0] = document.getElementById( "c0" ).value;
-        colors[1] = document.getElementById( "c1" ).value;
-        colors[2] = document.getElementById( "c2" ).value;
-        colors[3] = document.getElementById( "c3" ).value;
-        colors[4] = document.getElementById( "c4" ).value;
         tbl.innerHTML = '';
     }
     track1.playbackRate = speed;
     if( stems ) { track2.playbackRate = speed; }
-    let grd = ctx.createLinearGradient( 0, 0, 1000, 0 );
-        grd.addColorStop( 0.00, colors[1] );
-        grd.addColorStop( 0.25, colors[0] );
-        grd.addColorStop( 0.50, colors[0] );
-        grd.addColorStop( 0.75, colors[0] );
-        grd.addColorStop( 1.00, colors[1] );
-    return grd;
 }
 window.onload = function() {}
